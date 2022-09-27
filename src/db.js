089630fe960df33c6145330c0d72227dae787992
Gonzalo <zapozapico@gmail.com>
2022-09-27 15:56:52 -0300

    Fix Deploy Railway

--- src/db.js
+++ src/db.js
@@ -11,12 +11,12 @@ const {
 const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  // }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 const basename = path.basename(__filename);
 
