2dad6737d454b9ce8f1cfb223cbcb431cf4a023f
Gonzalo <zapozapico@gmail.com>
2022-09-27 13:59:58 -0300

    Get /

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // // Ejemplo: const authRouter = require('./auth.js');
-// const countryMiddleware = require("./Country.js");
-// const activityMiddleware = require("./Activity.js")
+const userMiddleware = require("./User.js");
+const productMiddleware = require("./Product.js")
 
 
 const router = Router();
@@ -10,10 +10,8 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-// router.use('/countries', countryMiddleware);
-// router.use('/activities', activityMiddleware);
+router.use('/products', productMiddleware);
+router.use('/users', userMiddleware);
 
-router.get("/", (req, res) => {
-    res.send("Levantado")
-})
+
 module.exports = router;
