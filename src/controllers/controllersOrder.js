738c857c5cbeb529221d57720272120a92c66bc1
Mevelezs <91708303+Mevelezs@users.noreply.github.com>
2022-09-29 04:27:24 -0500

    order routes

---
+++ src/controllers/controllersOrder.js
@@ -0,0 +1,30 @@
+const { Order,  User } = require("../db");
+
+const getAllOrders = async() =>{
+    const data = await Order.findAll({
+        include: {
+            model : User
+        }
+    })
+    return data;
+}
+
+const getGroupOrders = async() =>{
+    const orders = await Order.findAll({
+        group : 'orderNumber'
+    })
+    return orders;
+}
+
+const getOrderByDate = async (parametro) =>{
+    const data = await Order.findAll({
+        where : {
+            orderNumber : parametro
+        }})
+    return data;
+}
+module.exports = {
+    getAllOrders,
+    getGroupOrders,
+    getOrderByDate,
+}
