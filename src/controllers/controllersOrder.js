3747bd78a2e8d698ba0f562a7b69d028fdbe8ceb
Gonzalo <zapozapico@gmail.com>
2022-10-06 17:28:15 -0300

    Stripe test

--- src/controllers/controllersOrder.js
+++ src/controllers/controllersOrder.js
@@ -8,7 +8,7 @@ const getAllOrders = async() =>{
 const getGroupOrders = async() =>{
     const orders = await Order.findAll({
         group : 'orderNumber',
-        attributes: ["orderNumber", "productId", "userName", "amount"]
+        attributes: ["orderNumber", sequelize.fn('array_agg',sequelize.col('userName'), sequelize.col('productId'), sequelize.col('amount')),]
     })
     return orders;
 }
