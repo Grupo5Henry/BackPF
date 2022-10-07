39bab2f6d6ee304f93a7a9f7429d8a2999df4a94
Gonzalo <zapozapico@gmail.com>
2022-10-07 19:56:15 -0300

    Change to order timestamp model

--- src/models/Orders.js
+++ src/models/Orders.js
@@ -1,28 +1,38 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("order", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+  sequelize.define(
+    "order",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      orderNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      shippingAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("Pending", "Cancelled", "InDelivery", "Delivered"),
+        defaultValue: "Pending",
+        allowNull: true,
+      },
+      amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    orderNumber: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    shippingAddress: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM("Pending", "Cancelled", "InDelivery", "Delivered"),
-      defaultValue: "Pending",
-      allowNull: true,
-    },
-    amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: true,
+      updatedAt: false,
+      createdAt: {
+        type: DataTypes.DATEONLY,
+      },
+    }
+  );
 };
