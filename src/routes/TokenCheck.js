f4182ec8e915aee6d23903bb2b3d2c9131a9f6ec
Gonzalo <zapozapico@gmail.com>
2022-10-07 23:21:18 -0300

    Change to order timestamp model

--- src/routes/TokenCheck.js
+++ src/routes/TokenCheck.js
@@ -1,61 +1,51 @@
-
-const { Router } = require('express');
-const { User } = require('../db');
+const { Router } = require("express");
+const { User } = require("../db");
 
 const router = Router();
 const JWT = require("jsonwebtoken");
 const authToken = require("./middleware/authenticateToken");
 
 router.get("/tokenCheck", authToken, async (req, res) => {
-  return res
-    .status(200)
-    .send({
-      userName: req.userName,
-      defaultShippingAddress: req.defaultShippingAddress,
-      role: req.role,
-    });
+  return res.status(200).send({
+    userName: req.userName,
+    defaultShippingAddress: req.defaultShippingAddress,
+    role: req.role,
+  });
 });
 
-   
-router.get('/tokenRefresh', authToken, async (req,res)=>{
-  
- try{
-  const user = await User.findOne({
-    where: {
-      userName: req.user
-    }
-  })
-  
+router.get("/tokenRefresh", authToken, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        userName: req.user,
+      },
+    });
 
-  const accessToken = await JWT.sign(
-    { userName: user.userName,
-      role: user.role },
-    'ACCESS_TOKEN_SECRET',
-    {
+    const accessToken = await JWT.sign(
+      { userName: user.userName, role: user.role },
+      "ACCESS_TOKEN_SECRET",
+      {
         expiresIn: "3600s",
-    }
+      }
     );
 
     const refreshToken = await JWT.sign(
-        { userName: user.userName,
-          role: user.role },
-        'ACCESS_TOKEN_SECRET',
-        {
-            expiresIn: "3900s",
-        }
-        );
+      { userName: user.userName, role: user.role },
+      "ACCESS_TOKEN_SECRET",
+      {
+        expiresIn: "3900s",
+      }
+    );
 
     res.json({
-    accessToken, 
-    refreshToken,
-    privileges: user.role,
-    shippingAddress: user.defaultShippingAddress
+      accessToken,
+      refreshToken,
+      role: user.role,
+      shippingAddress: user.defaultShippingAddress,
     });
-} catch (err) {
-    res.send({error: err.message})
-}
+  } catch (err) {
+    res.send({ error: err.message });
+  }
 });
-
-
 
 module.exports = router;
