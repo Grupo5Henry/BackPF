4e00b3556a5d6cfc2cc8b60ae73e5c781e24b22d
Gonzalo <zapozapico@gmail.com>
2022-10-10 17:33:47 -0300

    Change for deployment

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -61,9 +61,7 @@ router.get("/login/failed", (req, res) => {
   });
 });
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }), {
-  withCredentials: true,
-});
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get(
   "/google/callback",
