e1dc3ad222f279a0350b4063726967355155bb6b
Gonzalo <zapozapico@gmail.com>
2022-10-12 16:56:47 -0300

    Check Stripe session id

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ const server = express();
 
 server.name = "API";
 
+app.use("/stripe/webhook", express.raw({ type: "*/*" }));
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
